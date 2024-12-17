8d0056e46f2c119be3a13f79bd48a8223f6f4cd4
dggalani <dggalani@ece.auth.gr>
2024-12-17 17:35:57 +0200

    change insecurity.ts

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -2,7 +2,6 @@
  * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
-
 import fs from 'fs'
 import crypto from 'crypto'
 import { type Request, type Response, type NextFunction } from 'express'
